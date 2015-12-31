aa0c533c5a8b8640e14c120c8920e9cde1f37604
Luke Fisher <mail@lukefisher.me>
2015-12-31 16:10:22 +0000

    Update Base Enums class to create DataRowVersion but not FileMode, FileAccess or UpdateRowSource on DNX Core

--- Source/MySql.Data/base/Enums.cs
+++ Source/MySql.Data/base/Enums.cs
@@ -1,23 +1,23 @@
 ﻿// Copyright © 2004, 2013, Oracle and/or its affiliates. All rights reserved.
 //
 // MySQL Connector/NET is licensed under the terms of the GPLv2
-// <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most 
-// MySQL Connectors. There are special exceptions to the terms and 
-// conditions of the GPLv2 as it is applied to this software, see the 
+// <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most
+// MySQL Connectors. There are special exceptions to the terms and
+// conditions of the GPLv2 as it is applied to this software, see the
 // FLOSS License Exception
 // <http://www.mysql.com/about/legal/licensing/foss-exception.html>.
 //
-// This program is free software; you can redistribute it and/or modify 
-// it under the terms of the GNU General Public License as published 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published
 // by the Free Software Foundation; version 2 of the License.
 //
-// This program is distributed in the hope that it will be useful, but 
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
 // for more details.
 //
-// You should have received a copy of the GNU General Public License along 
-// with this program; if not, write to the Free Software Foundation, Inc., 
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
 // 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
 
 using System;
@@ -29,7 +29,7 @@ using System.Text;
 
 namespace System.Data
 {
-#if !CF
+#if !CF && !DNXCORE50
     public enum UpdateRowSource
     {
       None,
@@ -37,7 +37,9 @@ namespace System.Data
       FirstReturnedRecord,
       Both
     }
+#endif
 
+#if !CF
     public enum DataRowVersion
     {
       Original = 256,
@@ -65,7 +67,7 @@ namespace System.ComponentModel
 
 namespace System.IO
 {
-#if !CF
+#if !CF && !DNXCORE50
   public enum FileMode
   {
     CreateNew = 1,
